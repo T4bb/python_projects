13f7cf5d686965b308155568a508ca7c80e1b23d
T4bb <108231842+T4bb@users.noreply.github.com>
2022-06-26 17:54:36 +0100

    Add files via upload

---
+++ updated vegan meal.py
@@ -0,0 +1,4 @@
+from random import choice
+#create a list of vegan food options
+food = ('spaghetti bolognese', 'salad', 'sticky tofu', 'lasagna', 'curry', 'burger', 'pad thai', 'quiche', 'nut roast', 'tacos', 'falafel wraps', 'pizza', 'pesto pasta', 'chicken and chips', 'roast aubergine' )
+print(choice(food))
